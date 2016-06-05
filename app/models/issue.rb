752234498b0e8e178f0e17b4d2d73099316ec8f2
xristocodos <xristojobos@gmail.com>
2016-06-05 16:12:00 -0400

    started work on sessions ctrllr

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,4 +1,6 @@
 class Issue < ApplicationRecord
   belongs_to :user
   has_many :options
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
+  get ‘/auth/:provider/callback’, to: 'sessions#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :issues do
     resources :options do
